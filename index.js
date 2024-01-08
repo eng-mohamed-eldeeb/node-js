0525968314a2c980adca10f959f445b6ada048f9
eng-mohamed-eldeeb <agnabiieldeeeb@gmail.com>
2024-01-08 21:11:13 +0200

    user asunc and awiat

--- index.js
+++ index.js
@@ -28,22 +28,41 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
+// this is using 'async' and await
+const goDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`the breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
+
+    const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
     console.log(res.body.message);
-    return writeFilePro(`${__dirname}/dog_image.txt`, res.body.message);
-    // fs.writeFile('dog_image.txt', res.body.message, (err) => {
-    //   if (err) console.log(err);
-    //   console.log('dog image saved 🔥👌');
-    // });
-  })
-  .then(() => {
-    console.log('dog image saved 🔥👌');
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+
+    await writeFilePro(`${__dirname}/dog_image.txt`, res.body.message);
+    console.log('dog image is saved');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+goDogPic();
+
+// this code is using 'then' and 'catch'
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`the breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+//     return writeFilePro(`${__dirname}/dog_image.txt`, res.body.message);
+//     // fs.writeFile('dog_image.txt', res.body.message, (err) => {
+//     //   if (err) console.log(err);
+//     //   console.log('dog image saved 🔥👌');
+//     // });
+//   })
+//   .then(() => {
+//     console.log('dog image saved 🔥👌');
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
